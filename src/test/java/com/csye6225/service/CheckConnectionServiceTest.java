b3d4eee93e6925c19c33773cd5f43fe2ab393ea5
Thejus Thomson <thomson.th@northeastern.edu>
2024-02-04 01:05:51 -0500

    [Assignment 2] - Fixing import vulnerabilities

--- src/test/java/com/csye6225/service/CheckConnectionServiceTest.java
+++ src/test/java/com/csye6225/service/CheckConnectionServiceTest.java
@@ -7,11 +7,8 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.util.Assert;
 
 import javax.sql.DataSource;
-
-import java.lang.reflect.Executable;
 import java.sql.Connection;
 import java.sql.SQLException;
 
