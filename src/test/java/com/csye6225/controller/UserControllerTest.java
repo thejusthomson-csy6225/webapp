7c0d3e734475ecf2f5c7daaa9339a1d30203d6a3
Thejus Thomson <thomson.th@northeastern.edu>
2024-02-14 00:25:24 -0500

    [Assignment 3] - Executing Integration test using Mock MVC

--- src/test/java/com/csye6225/controller/UserControllerTest.java
+++ src/test/java/com/csye6225/controller/UserControllerTest.java
@@ -1,30 +1,83 @@
 package com.csye6225.controller;
 
+import com.csye6225.model.User;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.After;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class UserControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void getUserDetails() {
+    void insertUserDetails_Success() throws Exception {
+        mockMvc.perform(post("/v1/user")
+                .content(createJsonInsert(new User(null, "Thejus", "Thomson", "password","thejus@gmail.com", null, null)))
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isCreated());
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBasicAuth("thejus@gmail.com", "password");
+        mockMvc.perform(get("/v1/user/self")
+                .headers(headers))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.username").value("thejus@gmail.com"))
+                .andExpect(jsonPath("$.firstName").value("Thejus"))
+                .andExpect(jsonPath("$.lastName").value("Thomson"))
+        ;
+
+        mockMvc.perform(delete("/v1/user/{username}", "thejus@gmail.com")
+        ).andExpect(status().isNoContent());
+
     }
 
     @Test
-    void insertUserDetails() {
+    void updateUserDetails_Success() throws Exception {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBasicAuth("thejus@gmail.com", "password");
+        mockMvc.perform(post("/v1/user")
+                .content(createJsonInsert(new User(null, "Thejus", "Thomson", "password","thejus@gmail.com", null, null)))
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isCreated());
+
+        mockMvc.perform(put("/v1/user/self")
+                .content(createJsonInsert(new User(null, "Wilson", "Jacob", "drowssap",null, null, null)))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+
+        headers.setBasicAuth("thejus@gmail.com", "drowssap");
+
+        mockMvc.perform(get("/v1/user/self")
+                        .headers(headers))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.firstName").value("Wilson"))
+                .andExpect(jsonPath("$.lastName").value("Jacob"))
+        ;
+
+        mockMvc.perform(delete("/v1/user/{username}", "thejus@gmail.com")
+        ).andExpect(status().isNoContent());
     }
 
-    @Test
-    void updateUserDetails() {
-    }
-
-    @Test
-    void handleHeadAndOptionsMethods() {
+    private String createJsonInsert(User user) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(user);
     }
 }
