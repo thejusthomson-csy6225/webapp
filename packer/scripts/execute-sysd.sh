dc8b52bd3b8f7f99983baf64f55f72b2ab6f9cf0
Thejus Thomson <thomson.th@northeastern.edu>
2024-02-21 06:01:49 -0500

    [Assignment 4] - Echoing to see if vars are running

--- packer/scripts/execute-sysd.sh
+++ packer/scripts/execute-sysd.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
+sudo chown csye6225:csye6225 /tmp/webapp-launch.service
 sudo cp /tmp/webapp-launch.service /etc/systemd/system/webapp-launch.service
 sudo systemctl daemon-reload
 sudo systemctl enable webapp-launch.service
