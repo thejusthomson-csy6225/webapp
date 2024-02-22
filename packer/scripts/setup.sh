9db03ba9240a57e6a8e22e49438505d6d9cf9bb3
Thejus Thomson <thomson.th@northeastern.edu>
2024-02-21 19:52:47 -0500

    [Assignment-4]- File not overriding

--- packer/scripts/setup.sh
+++ packer/scripts/setup.sh
@@ -9,8 +9,10 @@ sudo dnf install java-17-openjdk-devel.x86_64 -y
 #enable mysql
 sudo systemctl enable mysqld
 
+sleep 5
 #start service
 sudo systemctl start mysqld
 
+sleep 5
 #change password
 sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';"
