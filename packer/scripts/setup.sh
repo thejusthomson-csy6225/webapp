ba5d9f91443de4233df141f6caa83d6d3fcf6d6d
Thejus Thomson <thomson.th@northeastern.edu>
2024-02-21 19:07:37 -0500

    [Assignment-4]- SQL not starting error

--- packer/scripts/setup.sh
+++ packer/scripts/setup.sh
@@ -3,11 +3,14 @@
 #install mySQL
 sudo dnf install mysql-server -y
 
+#install java
+sudo dnf install java-17-openjdk-devel.x86_64 -y
+
+#enable mysql
+sudo systemctl enable mysqld
+
 #start service
 sudo systemctl start mysqld
-
-#install java 
-sudo dnf install java-17-openjdk-devel.x86_64 -y
 
 #change password
 sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';"
