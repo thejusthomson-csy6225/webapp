b86ce36c82759a6358912842d2962ac4b7072828
Thejus Thomson <thomson.th@northeastern.edu>
2024-02-29 13:24:06 -0500

    [Assignment-5]- Connecting to CloudSQL DB

--- packer/scripts/change-ownership.sh
+++ packer/scripts/change-ownership.sh
@@ -2,3 +2,5 @@
 
 # Set ownership of the copied JAR file
 sudo chown csye6225:csye6225 /tmp/webapp-0.0.1-SNAPSHOT.jar
+
+sudo mv /tmp/webapp-0.0.1-SNAPSHOT.jar /opt/webapp-0.0.1-SNAPSHOT.jar
--- packer/scripts/create-user.sh
+++ packer/scripts/create-user.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/bash
 
-#create group csye6225
-
 #Create user csye6225
 sudo adduser csye6225 --shell /usr/sbin/nologin
